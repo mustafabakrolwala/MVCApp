f0833fca9021cb459e06d91d0d660ff31367fdaf
mustafab <mustafab@cybage.com>
2016-01-06 11:08:09 +0530

    initial commit

---
+++ SampleContacts/SampleContacts/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using SampleContacts.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleContacts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
