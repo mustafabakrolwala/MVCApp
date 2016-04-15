487b688270abcb5acfb567b0a1280d1a3eedee5f
mustafab <mustafab@cybage.com>
2016-04-15 12:26:48 +0530

    fhsjfhjdfh

--- SampleContacts/SampleContacts/Contact.cs
+++ SampleContacts/SampleContacts/Contact.cs
@@ -14,6 +14,7 @@ namespace SampleContacts
     
     public partial class Contact
     {
+	//
         public int ContactId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
